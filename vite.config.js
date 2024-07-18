bf92a427d13319ef50f41f0dedb81f3e014d2279
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-07-18 17:58:31 +0500

    1111

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  // server: { open: true, port: 5173, },
-  build: {
-    resolve: {
-      alias: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
 })
