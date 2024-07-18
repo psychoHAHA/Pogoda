63b23afabca308ffbddd6917e64aaddfa898817c
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-07-18 17:35:22 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: "/pogoda/",
+  // server: { open: true, port: 5173, },
+  build: {
+    resolve: {
+      alias: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
